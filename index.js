80e5eba720aaf3fd976b48f973da9c57cf02a01c
Kasyan Roman <kasanr223@gmail.com>
2023-09-23 10:58:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/*a = {1:22,2:99}
+
+console.log(a[1])
+console.log(a[2])
+
+console.log(a[1] + a[2])*/
+
+
+let str = 'jyatwdfjyawdftadwtfdadwtheaghwad'
+
+if(str.length < 8){
+    console.log('Small')
+}
+
+else{
+    console.log('big')
+}
+
